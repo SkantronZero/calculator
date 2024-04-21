be39b0e0141c929cf00798a3c99c6da98a6f20fc
SkantronZero <150893003+SkantronZero@users.noreply.github.com>
2024-04-20 18:12:43 -0600

    initial design

--- script.js
+++ script.js
@@ -1,8 +1,20 @@
-import "functions.js";
+/*
+Initialization
+*/
+
+// let test = document.createElement('button');
+// test.textContent = 'test';
+// document.body.appendChild(test);
+
+// testFunction();
 
 
-let test = document.createElement('button');
-test.textContent = 'test';
-document.body.appendChild(test);
 
-testFunction();
+
+
+/*
+
+Functions
+
+*/
+
