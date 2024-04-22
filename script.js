e76f00110a1cacaecf5e4213172db7099832681c
SkantronZero <150893003+SkantronZero@users.noreply.github.com>
2024-04-22 16:18:24 -0600

    design goal finished

--- script.js
+++ script.js
@@ -2,15 +2,18 @@
 Initialization
 */
 
-// let test = document.createElement('button');
-// test.textContent = 'test';
-// document.body.appendChild(test);
-
-// testFunction();
+let calculator_display_inner_text = document.getElementById("calculator_display_inner_text");
+calculator_display_inner_text.textContent = '102150';
 
 
+let AC_button = document.getElementById("AC_button");
 
 
+
+
+let seven_button = document.getElementById("seven_button");
+
+seven_button.addEventListener("click", updateDisplay);
 
 /*
 
@@ -18,3 +21,6 @@ Functions
 
 */
 
+function updateDisplay(){
+    calculator_display_inner_text.textContent = "hello world";
+}
