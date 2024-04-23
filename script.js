db643fdac42d3083f39c874ab73c275002a7c6a6
SkantronZero <150893003+SkantronZero@users.noreply.github.com>
2024-04-23 01:02:17 -0600

    Key pad support and display update

--- script.js
+++ script.js
@@ -1,17 +1,12 @@
 /*
 Initialization
 */
+let numRegEx = '[0-9.]';
 
 let calculator_display_inner_text = document.getElementById("calculator_display_inner_text");
-calculator_display_inner_text.textContent = '102150';
+calculator_display_inner_text.textContent = '';
 let AC_button = document.getElementById("AC_button");
 
-
-
-
-let seven_button = document.getElementById("seven_button");
-
-seven_button.addEventListener("click", updateDisplay);
 
 /*
 
@@ -19,6 +14,59 @@ Functions
 
 */
 
-function updateDisplay(){
-    calculator_display_inner_text.textContent = "hello world";
+function inputCollection(event){
+    console.log(event.key);
+    if(event.key == 'Backspace'){
+        let str = calculator_display_inner_text.textContent;
+        str = str.slice(0, -1);
+        calculator_display_inner_text.textContent = str;
+    }
+    if(typeof(event.key) === 'string' && event.key.match(numRegEx)){
+        calculator_display_inner_text.textContent += event.key;
+    }
+    if(event.key == 'Delete'){
+        clearCollectionAndDisplay();
+    }
+    if(event.key == 'x' || event.key == '*'){
+        console.log('multiply');
+    }
+    if(event.key == '/'){
+        console.log('divide');
+    }
+    if(event.key == '%'){
+        console.log('remainder');
+    }
+    if(event.key == '-'){
+        console.log('subtract');
+    }
+    if(event.key == '+'){
+        console.log('add');
+
+    }
+    if(event.key == '=' || event.key == 'Enter'){
+        console.log('equals');
+    }
 }
+
+function clearCollectionAndDisplay(){
+
+    calculator_display_inner_text.textContent = '';
+
+}
+
+function evaluate(x, op, y){
+    // calculator_display_inner_text.textContent = 
+}
+
+
+
+
+/*
+
+Execution
+
+*/
+
+window.addEventListener('keydown', inputCollection);
+AC_button.addEventListener('click', clearCollectionAndDisplay);
+
